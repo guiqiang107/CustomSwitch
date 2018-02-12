50957afd0a197cef40a1c91904d35405b4779c16
guiqiang107 <guiqiang107@gmail.com>
2018-02-12 14:33:10 +0800

    自定义实现一个系统的UISwitch

--- CustomSwitch/ViewController.swift
+++ CustomSwitch/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var customSwitch: CustomSwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.customSwitch = CustomSwitch()
+        self.view.addSubview(customSwitch)
+        customSwitch.center = self.view.center
+        customSwitch.bounds = CGRect(x: 0, y: 0, width: 40, height: 30)
+        customSwitch.valueChangedHandle = {(isOn) in
+            print(isOn)
+        }
+        
+        var config = SwitchConfig()
+        config.offBgColor = UIColor.red
+        customSwitch.config = config
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
